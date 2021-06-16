d7625b9eccdb2133362ab996c5131115c6381c6c
Vishesh Arora <>
2021-06-16 14:16:13 +0800

    Write testing center data

---
+++ src/assets/data/testingCenters.js
@@ -0,0 +1,30 @@
+export const testingCenters = [
+  {
+    name: "Former Shu Qun Secondary School",
+    country: "Singapore",
+    latitude: 1.3403223663047692,
+    longitude: 103.73949126825953,
+    address: "450 Jurong East Street 21, Singapore 609604",
+  },
+  {
+    name: "Former Coral Primary School",
+    country: "Singapore",
+    latitude: 1.3679728449527087,
+    longitude: 103.95083041034623,
+    address: "20 Pasir Ris Street 51, Singapore 518902",
+  },
+  {
+    name: "Former Da Qiao Primary School",
+    country: "Singapore",
+    latitude: 1.3719543382857247,
+    longitude: 103.8586789117008,
+    address: "8 Ang Mo Kio Street 54, Singapore 569185",
+  },
+  {
+    name: "Former Bedok North Secondary",
+    country: "Singapore",
+    latitude: 1.3342515410280846,
+    longitude: 103.91033340465256,
+    address: "20 Jalan Damai, Singapore 419612",
+  },
+];
