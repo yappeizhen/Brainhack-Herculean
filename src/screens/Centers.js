44f0b8f163a4f9558e9e79c79b19ed7af76d8cf8
Vishesh Arora <>
2021-06-16 19:23:38 +0800

    Toggle images when clicked on

--- src/screens/Centers.js
+++ src/screens/Centers.js
@@ -14,23 +14,66 @@ import {
 import { testingCenters } from "../assets/data/testingCenters";
 
 const TestingCentersRoute = () => {
-  const [selectedTestingCenter, setSelectedTestingCenter] = useState();
+  const [selectedTestingCenter, setSelectedTestingCenter] = useState(null);
 
   const handleSelectedCard = (testingCenter) => {
     setSelectedTestingCenter(testingCenter);
   };
 
   const getImgSrc = (selectedTestingCenter) => {
-    if (selectedTestingCenter.name === "Former Shu Qun Secondary School") {
-      return "https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg";
+    if (selectedTestingCenter === null) {
+      return "../images/419612.png";
+    } else if (
+      selectedTestingCenter.name === "Former Shu Qun Secondary School"
+    ) {
+      return "../images/609604.png";
     } else if (selectedTestingCenter.name === "Former Coral Primary School") {
-      return "https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/dog_cool_summer_slideshow/1800x1200_dog_cool_summer_other.jpg";
+      return "../images/518902.png";
     } else if (selectedTestingCenter.name === "Former Da Qiao Primary School") {
-      return "https://images.theconversation.com/files/319375/original/file-20200309-118956-1cqvm6j.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=900.0&fit=crop";
+      return "../images/569185.png";
     } else if (selectedTestingCenter.name === "Former Bedok North Secondary") {
-      return "https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg";
+      return "../images/419612.png";
+    }
+  };
+
+  const DisplayAnImage = () => {
+    if (selectedTestingCenter === null) {
+      return (
+        <Image
+          style={styles.image}
+          source={require("../images/sg.png")}
+        ></Image>
+      );
+    } else if (
+      selectedTestingCenter.name === "Former Shu Qun Secondary School"
+    ) {
+      return (
+        <Image
+          style={styles.image}
+          source={require("../images/609604.png")}
+        ></Image>
+      );
+    } else if (selectedTestingCenter.name === "Former Coral Primary School") {
+      return (
+        <Image
+          style={styles.image}
+          source={require("../images/518902.png")}
+        ></Image>
+      );
+    } else if (selectedTestingCenter.name === "Former Da Qiao Primary School") {
+      return (
+        <Image
+          style={styles.image}
+          source={require("../images/569185.png")}
+        ></Image>
+      );
     } else {
-      return "https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/dog_cool_summer_slideshow/1800x1200_dog_cool_summer_other.jpg";
+      return (
+        <Image
+          style={styles.image}
+          source={require("../images/419612.png")}
+        ></Image>
+      );
     }
   };
 
@@ -57,12 +100,7 @@ const TestingCentersRoute = () => {
           onPress={() => console.log("Search places")}
         />
       </Appbar.Header>
-      <Image
-        style={styles.image}
-        source={{
-          uri: getImgSrc(setSelectedTestingCenter),
-        }}
-      ></Image>
+      {DisplayAnImage()}
       <ScrollView contentContainerStyle={styles.scrollableSection}>
         <Title style={{ width: "100%", paddingLeft: 20 }}>
           Testing Centers
