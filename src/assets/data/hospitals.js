c5c4ae4ec89aa606fc3419a44f0ddd555277a765
Vishesh Arora <>
2021-06-16 22:51:48 +0800

    Add hospitals data

---
+++ src/assets/data/hospitals.js
@@ -0,0 +1,17 @@
+export const hospitals = [
+  {
+    name: "Singapore General Hospital",
+    country: "Singapore",
+    address: "450 Jurong East Street 21, Singapore 609604",
+  },
+  {
+    name: "Chang General Hospital",
+    country: "Singapore",
+    address: "20 Pasir Ris Street 51, Singapore 518902",
+  },
+  {
+    name: "National University Hospital",
+    country: "Singapore",
+    address: "8 Ang Mo Kio Street 54, Singapore 569185",
+  },
+];
